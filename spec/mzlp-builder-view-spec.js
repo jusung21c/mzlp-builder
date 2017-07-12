08fe025b61fef2e5b1cbd7fa04f2c2a3c2004ab9
JuicyJusung <jusung21c@gmail.com>
2017-07-12 19:22:13 +0900

    FR
    빌더패스 설정

---
+++ spec/mzlp-builder-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MzlpBuilderView from '../lib/mzlp-builder-view';
+
+describe('MzlpBuilderView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
